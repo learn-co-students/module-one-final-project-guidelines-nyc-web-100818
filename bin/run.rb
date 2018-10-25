278d082ff5fb981bebbb784e47a895a28408e830
aaaaus <ausluft@gmail.com>
2018-10-25 15:49:23 -0400

    added methods, fixed class methods

--- bin/run.rb
+++ bin/run.rb
@@ -8,7 +8,6 @@ api = API.new
 selection = api.city_selection
 
 hash = api.get_response(selection)
-# binding.pry
 
 api.populate_db(hash)
 
@@ -16,11 +15,17 @@ current_city = api.selected_city(selection)
 
 # binding.pry
 
-api.city_today_info(current_city, hash)
+today = today_weather(current_city)
+
+api.city_today_info(current_city, today)
 
 api.week_forecast(current_city)
 
 menu2
+
+# least_rain_forecast(current_city)
+
+today_weather(current_city)
 
 binding.pry
 
